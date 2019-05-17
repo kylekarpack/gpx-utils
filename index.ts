9105df5f16974a33d3729f3289825060a8f900a0
Kyle Karpack <kylekarpack@gmail.com>
2019-05-17 08:35:04 -0700

    Added some basic examples

--- index.ts
+++ index.ts
@@ -1,42 +1,59 @@
 import * as toGeoJson from "@mapbox/togeojson";
 import { Units } from "@turf/helpers";
 import length from "@turf/length";
-import { GpxFile } from "./interfaces/gpx-file";
-import { Trkpt } from "./interfaces/trkpt";
-import { DOMParser } from "xmldom";
+import { GpxFile } from "./classes/gpx-file";
+import * as gpxParse from "gpx-parse";
 import * as fs from "fs";
-
-import { TIME_SELECTOR, TRACK_NAME_SELECTOR, TRACK_POINTS_SELECTOR } from "./constants/selectors";
+import { GpxWaypoint } from 'classes/gpx-waypoint';
 
 export class GpxUtils {
 
-	get points(): Trkpt[] {
-		console.log(this.gpxFile)
-		return this.gpxFile.trk.trkseg.trkpt;
+	get points(): GpxWaypoint[] {
+		return this.gpxFile.tracks[0].segments[0];
 	}
 
 	get geoJson() {
-		return toGeoJson(this.gpxFile);
+		return toGeoJson(this.gpxFile.tracks[0].segments[0]);
 	}
 
-	constructor(private gpxFile: GpxFile) { }
+	constructor(public gpxFile: GpxFile) { }
 
-	public static fromFile(fileData: string): GpxUtils {
-		const parsedFile = this.parseFile(fileData);
+	public static async fromFile(fileData: string): Promise<GpxUtils> {
+		const parsedFile = await this.parseFile(fileData);
 		return new GpxUtils(parsedFile);
 	}
 
-	public static fromFilePath(filePath: string): GpxUtils {
-		return this.fromFile(fs.readFileSync(filePath).toString());
+	public static async fromFilePath(filePath: string): Promise<GpxUtils> {
+		return await this.fromFile(fs.readFileSync(filePath).toString());
 	}
 
-	public static parseFile(fileData: string): GpxFile {
-		return new DOMParser().parseFromString(fileData, "text/xml");
+	public static async parseFile(fileData: string): Promise<GpxFile> {
+		return new Promise((resolve, reject) => {
+			gpxParse.parseGpx(fileData, (err, data) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(data);
+			});
+		});
 	}
 
 
 	public getTotalTime(): number {
 		return this.points[this.points.length - 1].time.getTime() - this.points[0].time.getTime();
+	}
+
+	public getMovingTime(): number {
+		let time: number = 0,
+			prevPoint: GpxWaypoint = this.points[0];
+		for (let point of this.points) {
+			if (point.lat !== prevPoint.lat || point.lon !== prevPoint.lon || point.elevation !== prevPoint.elevation) {
+				time = time + (point.time.getTime() - prevPoint.time.getTime());
+			}
+			prevPoint = point;
+		}
+		return time;
+
 	}
 
 	public getTotalDistance(options: { units: Units } = { units: "miles" }): number {
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
-const { GpxUtils } = require("./index"),
+const { GpxUtils } = require("./dist/index"),
 	fs = require("fs");
 
 let file = fs.readFileSync("./beckler-peak.gpx").toString();
 
-let gpx = GpxUtils.fromFile(file);
 
-console.log(gpx.getTotalTime());
+(async function go() {
+	let gpx = await GpxUtils.fromFile(file);
+	console.log("total time", gpx.getTotalTime());
+	console.log("moving time", gpx.getMovingTime());
+})();
