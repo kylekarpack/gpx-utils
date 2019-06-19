fc28a5cc5bd4eb47dfa1289796914dc6a6a09dfe
Kyle Karpack <kylekarpack@gmail.com>
2019-06-19 08:33:16 -0700

    Reorganized code and added comments

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import center from "@turf/center";
-import { AllGeoJSON, Units, point, Point } from "@turf/helpers";
+import { AllGeoJSON, Units, point, Point, Feature } from "@turf/helpers";
 import length from "@turf/length";
 import distance from "@turf/distance";
 import { GpxWaypoint } from "classes/gpx-waypoint";
@@ -25,15 +25,32 @@ export class GpxUtils {
 		this.geoJson = new GeoJson(gpxFile).geoJson;
 	}
 
+	//#region Initialization functions
+	
+	/**
+	 * Create a GpxUtils class from the string representation of a GPX file
+	 * @param  {string} fileData string contents of a GPX file
+	 * @returns {Promise<GpxUtils>} instance of this class
+	 */
 	public static async fromFile(fileData: string): Promise<GpxUtils> {
 		const parsedFile = await this.parseFile(fileData);
 		return new GpxUtils(parsedFile);
 	}
 
+	/**
+	 * Create a GpxUtils class from the path to the file
+	 * @param  {string} filePath 
+	 * @returns {Promise<GpxUtils>} instance of this class
+	 */
 	public static async fromFilePath(filePath: string): Promise<GpxUtils> {
 		return await this.fromFile(fs.readFileSync(filePath).toString());
 	}
-
+	
+	/**
+	 * Parse GPX file text content to JSON
+	 * @param  {string} fileData
+	 * @returns {Promise<GpxFile>} a GPX file in JSON format
+	 */
 	public static async parseFile(fileData: string): Promise<GpxFile> {
 		return new Promise((resolve, reject) => {
 			gpxParse.parseGpx(fileData, (err, data) => {
@@ -45,16 +62,74 @@ export class GpxUtils {
 		});
 	}
 
-	private waypointToPoint(waypoint: GpxWaypoint): any {
+	//#endregion
+
+	//#region Private helper functions
+	
+	/**
+	 * Convert a GPX Waypoint to a GeoJSON Point object
+	 * @param  {GpxWaypoint} waypoint
+	 * @returns {Feature} output point
+	 */
+	private waypointToPoint(waypoint: GpxWaypoint): Feature<Point, { [name: string]: any }> {
 		return point([waypoint.lon, waypoint.lat]);
 	}
+	
+	/**
+	 * Run an aggregation function (metricFn) over a set of matching points (compareFn)
+	 * @param  {(a:GpxWaypoint,b:GpxWaypoint)=>boolean} compareFn used to filter for matching points
+	 * @param  {(a:GpxWaypoint,b:GpxWaypoint)=>any} metricFn used to calculate the metric
+	 * @returns {T} the output metric
+	 */
+	private getMetricOverMatchingPoints<T>(
+		compareFn: (a: GpxWaypoint, b: GpxWaypoint) => boolean,
+		metricFn: (a: GpxWaypoint, b: GpxWaypoint) => any
+	): T {
+		let metric = 0;
 
-	private getClimbingParts(): GpxWaypoint[] {
-		return this.points.filter((waypoint, i) => {
-			return this.points[i] && this.points[i + 1] && this.points[i].elevation < this.points[i + 1].elevation;
-		});
+		for (let i = 0; i < this.points.length; i++) {
+			if (this.points[i] && this.points[i + 1]) {
+				if (compareFn(this.points[i], this.points[i + 1])) {
+					metric += metricFn(this.points[i], this.points[i + 1]);
+				}
+			}			
+		}
+
+		return metric as any;
 	}
 
+	/**
+	 * Aggregate points based on the distance between them
+	 * @param  {GpxWaypoint} currentWaypoint
+	 * @param  {GpxWaypoint} nextWaypoint
+	 * @returns {number} the distance between the waypoints
+	 */
+	private distanceMetric = (currentWaypoint: GpxWaypoint, nextWaypoint: GpxWaypoint): number => {
+		return distance(
+			this.waypointToPoint(currentWaypoint), 
+			this.waypointToPoint(nextWaypoint), 
+			{ units: "miles" }
+		);
+	}
+
+	/**
+	 * Aggregate points based on the time between them
+	 * @param  {GpxWaypoint} currentWaypoint
+	 * @param  {GpxWaypoint} nextWaypoint
+	 * @returns number
+	 */
+	private timeMetric = (currentWaypoint: GpxWaypoint, nextWaypoint: GpxWaypoint): number => {
+		return nextWaypoint.time.getTime() - currentWaypoint.time.getTime();
+	}
+
+	//#endregion
+
+	//#region Public API Surface
+	
+	/**
+	 * Get the total time elapsed during the track
+	 * @returns {number} the time of the track in milliseconds
+	 */
 	@memoize()
 	public getTotalTime(): number {
 		return this.points[this.points.length - 1].time.getTime() - this.points[0].time.getTime();
@@ -86,7 +161,6 @@ export class GpxUtils {
 
 	@memoize()
 	public getNetTotalElevation(): number {
-
 		return this.points.reduce((total, currentItem, index) => {
 			const nextItem = this.points[index + 1];
 			if (nextItem && nextItem.elevation > currentItem.elevation) {
@@ -106,31 +180,6 @@ export class GpxUtils {
 		return center(this.geoJson).geometry.coordinates;
 	}
 
-	private getMetricOverMatchingPoints<T>(
-		compareFn: (a: GpxWaypoint, b: GpxWaypoint) => boolean,
-		metricFn: (a: GpxWaypoint, b: GpxWaypoint) => any
-	): T {
-		let metric = 0;
-
-		for (let i = 0; i < this.points.length; i++) {
-			if (this.points[i] && this.points[i + 1]) {
-				if (compareFn(this.points[i], this.points[i + 1])) {
-					metric += metricFn(this.points[i], this.points[i + 1]);
-				}
-			}			
-		}
-
-		return metric as any;
-	}
-
-	private distanceMetric = (a: GpxWaypoint, b: GpxWaypoint): number => {
-		return distance(this.waypointToPoint(a), this.waypointToPoint(b), { units: "miles" });
-	}
-
-	private timeMetric = (a: GpxWaypoint, b: GpxWaypoint): number => {
-		return b.time.getTime() - a.time.getTime()
-	}
-
 	@memoize()
 	public getClimbingDistance(): number {
 		const matchingFunction = (a: GpxWaypoint, b: GpxWaypoint) => a.elevation < b.elevation;
@@ -143,7 +192,7 @@ export class GpxUtils {
 		return this.getMetricOverMatchingPoints<number>(matchingFunction, this.distanceMetric);
  	}
 
-	 @memoize()
+	@memoize()
 	 public getFlatDistance(): number {
 		 const matchingFunction = (a: GpxWaypoint, b: GpxWaypoint) => a.elevation === b.elevation;
 		 return this.getMetricOverMatchingPoints<number>(matchingFunction, this.distanceMetric);
@@ -181,5 +230,7 @@ export class GpxUtils {
 	public getFlatSpeed(): number {
 		return this.getFlatDistance() / (this.getFlatTime() / 1000 / 60 / 60);
 	}
+
+	//#endregion
 
 }
